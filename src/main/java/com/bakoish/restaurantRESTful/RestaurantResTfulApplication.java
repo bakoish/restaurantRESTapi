cdcbaa1789235f5c6e168967deff44b91b335fa3
Bartosz <bakoish@gmail.com>
2022-10-16 17:16:43 +0200

    Init commit

---
+++ src/main/java/com/bakoish/restaurantRESTful/RestaurantResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.bakoish.restaurantRESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantResTfulApplication.class, args);
+	}
+
+}
